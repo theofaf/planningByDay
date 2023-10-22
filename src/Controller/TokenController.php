9ca3a051a16e270a283d8f0da3a28f97df45dcf1
theofaf <theophane.fafet2021@campus-eni.fr>
2023-10-22 20:02:28 +0200

    refacto TokenController.php

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -10,9 +10,15 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class TokenController extends AbstractController
 {
+    public function __construct(
+        private readonly SerializerInterface  $serializer,
+    ) {
+    }
+
     /**
      * @OA\Post(
      *     path="/api/validate/token",
@@ -55,6 +61,9 @@ class TokenController extends AbstractController
             throw new AuthenticationException('Bearer Token not provided.');
         }
 
-        return new JsonResponse(['valid' => true]);
+        return new JsonResponse([
+            'valid' => true,
+            'user' => $this->serializer->serialize($this->getUser(), 'json', ['groups' => 'utilisateur']),
+        ]);
     }
 }
