d95664b449992fdacf16a39a2b0bf018d34cc782
Kilian Naudin <kilian.naudin22@gmail.com>
2023-10-22 11:40:29 +0200

    token ok

--- src/Controller/TokenController.php
+++ src/Controller/TokenController.php
@@ -2,48 +2,15 @@
 
 namespace App\Controller;
 
-
-use FOS\RestBundle\Controller\Annotations as Rest;
-use OpenApi\Annotations as OA;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Nelmio\ApiDocBundle\Annotation\Security;
-use Nelmio\ApiDocBundle\Annotation\Model;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
 
 class TokenController extends AbstractController
 {
-    /**
-     * @OA\Post(
-     *     path="/api/validate/token",
-     *     tags={"Authentification"},
-     *     summary="Valider un token d'authentification Bearer",
-     *     @OA\Parameter(
-     *          name="Authorization",
-     *          in="header",
-     *          required=true,
-     *          description="Le token d'authentification Bearer",
-     *          @OA\Schema(type="string")
-     *      ),
-     *     @OA\Response(
-     *         response=200,
-     *         description="Token valide",
-     *         @OA\JsonContent(
-     *             type="object",
-     *             @OA\Property(property="valid", type="boolean", example=true)
-     *         )
-     *     ),
-     *     @OA\Response(
-     *         response=401,
-     *         description="Token Bearer non fourni ou invalide"
-     *     )
-     * )
-     *
-     * @Rest\Post("/api/validate/token")
-     * @Security(name="Bearer")
-     */
+    #[Route('/api/token/validate', name: 'validate_token', methods: "POST")]
     public function validateToken(Request $request): JsonResponse
     {
         $authorizationHeader = $request->headers->get('Authorization');
@@ -54,7 +21,7 @@ class TokenController extends AbstractController
         }
 
         if (!$token) {
-            return new JsonResponse('token invalid', Response::HTTP_UNAUTHORIZED);
+            throw new AuthenticationException('Bearer Token not provided.');
         }
 
         return new JsonResponse(['valid' => true]);
